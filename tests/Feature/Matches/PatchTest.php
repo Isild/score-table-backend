a9e456268b110bf0563e7f9520f3ca0ed9840b34
Isild <pimok96@gmail.com>
2023-10-18 02:38:21 +0200

    [matches] added feature tests for patch route

---
+++ tests/Feature/Matches/PatchTest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Tests\Feature\Teams;
+
+use App\Models\FootballMatch;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PatchTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected $validPayload = [];
+
+    protected $dataToCreate = [];
+
+    protected $invalidPayload = [];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->validPayload = [
+            'home_team_score' => 1,
+            'away_team_score' => 2,
+        ];
+
+        $this->invalidPayload = [
+            'home_team_score' => [
+                -1,
+                false,
+                true,
+                null,
+                "a",
+            ],
+            'away_team_score' => [
+                -1,
+                false,
+                true,
+                null,
+                "a",
+            ],
+        ];
+
+        $this->dataToCreate = [
+            'home_team_score' => 0,
+            'away_team_score' => 0,
+            'total_match_score' => 0,
+            'total_time' => '00:00:00',
+        ];
+    }
+
+    public function test_patch(): void
+    {
+        $model = FootballMatch::factory()->create($this->dataToCreate);
+        $this->assertDatabaseHas('football_matches', $this->dataToCreate);
+
+        $response = $this->patch(route('matches.update-score', $model), $this->validPayload);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('football_matches', $this->validPayload);
+        $this->assertDatabaseMissing('football_matches', $this->dataToCreate);
+    }
+
+    public function test_patch_finished(): void
+    {
+        $this->dataToCreate = [];
+        $model = FootballMatch::factory()->create($this->dataToCreate);
+        $this->assertDatabaseHas('football_matches', $this->dataToCreate);
+
+        $response = $this->patch(route('matches.update-score', $model), $this->validPayload);
+
+        $response->assertStatus(403);
+        $this->assertDatabaseHas('football_matches', $this->dataToCreate);
+    }
+
+    public function test_patch_not_found(): void
+    {
+        $response = $this->patch(route('matches.active_matches') . "/99999/score", $this->validPayload);
+
+        $responseData = json_decode($response->getContent(), true);
+
+        $response->assertStatus(404);
+        $this->assertEquals('Resource not found.', $responseData['message']);
+    }
+
+    public function test_patch_invalid_input(): void
+    {
+        foreach ($this->invalidPayload as $key => $values) {
+            foreach ($values as $value) {
+                $response = $this->patch(route('matches.update-score'), [
+                    $key => $value,
+                ]);
+
+                $response->assertStatus(422);
+            }
+        }
+    }
+}
